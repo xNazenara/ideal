c8dda4f92d46b4069f597668dcfee79c618d706d
xNazenara <kanlandball553@gmail.com>
2022-02-13 19:35:05 +0300

    split page in two parts with different colors

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "4cfb0a9604e1fad5"; }
+/******/ 		__webpack_require__.h = function() { return "905324396e436a60"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,28 @@
 const HomePage = () => {
-  return <div>Welcome to Next.js!</div>
+  return <div style={{display: "flex"}}>
+<div style={{
+width: "50%",
+background:"red",
+color: "white",
+height: "100px",
+display: "flex",
+alignItems:"center",
+justifyContent:"center"
+}}>
+Elisey
+</div>
+<div style={{
+width: "50%",
+background:"blue",
+color: "white",
+height: "100px",
+display: "flex",
+alignItems:"center",
+justifyContent:"center"
+}}>
+Dima
+</div>
+  </div>
 }
 
 export default HomePage
